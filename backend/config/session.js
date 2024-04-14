587130d5a1b40517d6eb9d93c98ab67def172e41
Nicolas Ollier <nicolas@lepetitmartin.com>
2024-04-14 16:40:27 +0200

    fix session params

---
+++ backend/config/session.js
@@ -0,0 +1,8 @@
+const params = {
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true },
+}
+
+module.exports = params
--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const session = require('express-session');
+const params = require('./config/session.js')
 const helmet = require('helmet');
 const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const session_params = {
-  secret: 'your_secret_key',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true },
-}
 
 app.use(helmet());
-app.use(session(session_params));
+app.use(session(params));
 app.use(passport.initialize());
 app.use(passport.session());
 
